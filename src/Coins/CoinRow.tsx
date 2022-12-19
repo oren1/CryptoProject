c7013735daa01e9c71fcb631187cb4093ebd0b15
oren shalev <orenshalev@orens-MacBook-Pro.local>
2022-12-19 09:23:02 +0200

    first commit

---
+++ src/Coins/CoinRow.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {View, Text, StyleSheet, TouchableHighlight} from 'react-native';
+import FastImage from 'react-native-fast-image';
+
+interface CoinRowProp {
+  coinName: string;
+  imageUrl: string;
+  onPress: () => void;
+}
+
+const CoinRow = ({onPress, coinName, imageUrl}: CoinRowProp) => {
+  const CoinView = () => {
+    return (
+      <View style={styles.coinRowContainer}>
+        <FastImage source={{uri: imageUrl}} style={{width: 75, height: 75}} />
+
+        <Text style={styles.titleText}>{coinName}</Text>
+      </View>
+    );
+  };
+
+  return (
+    <TouchableHighlight
+      activeOpacity={0.6}
+      underlayColor="#DDDDDD"
+      onPress={onPress}>
+      <CoinView />
+    </TouchableHighlight>
+  );
+};
+
+const styles = StyleSheet.create({
+  coinRowContainer: {
+    margin: 0,
+    flexDirection: 'row',
+  },
+  titleText: {
+    flex: 1,
+    margin: 10,
+    alignSelf: 'center',
+    fontSize: 20,
+    fontWeight: '500',
+  },
+});
+
+export default CoinRow
